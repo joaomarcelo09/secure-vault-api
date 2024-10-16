63e06b7310f8e73802fa4f8b8f4eeb50be0b7bbd
Joao Gomes <jg0112360@gmail.com>
2024-10-15 23:20:57 -0300

    Initial commit

---
+++ src/main/java/com/security_vault/SecurityVaultApplication.java
@@ -0,0 +1,13 @@
+package com.security_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security_vault/SecurityVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
