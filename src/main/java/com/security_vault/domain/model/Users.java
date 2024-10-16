740ae33d027cc38a092abb8021d7d7c4bc486aeb
Joao Gomes <jg0112360@gmail.com>
2024-10-16 19:11:06 -0300

    feat(database): new table and entity user created

---
+++ src/main/java/com/security_vault/domain/model/Users.java
@@ -0,0 +1,31 @@
+package com.security_vault.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
