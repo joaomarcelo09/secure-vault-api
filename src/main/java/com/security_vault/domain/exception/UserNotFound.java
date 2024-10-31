c727e072ac47cfb927d96f56b6872c931c8057a7
Joao Gomes <jg0112360@gmail.com>
2024-10-31 17:34:05 -0300

    feat(exception): add a global handler exception

--- src/main/java/com/security_vault/domain/exception/UserNotFound.java
+++ src/main/java/com/security_vault/domain/exception/UserNotFound.java
@@ -1,7 +1,5 @@
 package com.security_vault.domain.exception;
 
-import java.util.function.Supplier;
-
 public class UserNotFound extends RuntimeException {
     public UserNotFound() {super("User not found");}
 }
