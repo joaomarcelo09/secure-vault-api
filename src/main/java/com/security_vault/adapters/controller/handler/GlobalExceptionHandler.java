d2f24fe507d82812de4446a8df2a9ad6b9ac91cc
Joao Gomes <jg0112360@gmail.com>
2024-10-31 17:33:50 -0300

    feat(exception): add a global handler exception

--- src/main/java/com/security_vault/adapters/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/com/security_vault/adapters/controller/handler/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package com.security_vault.adapters.controller.handler;
 import com.security_vault.domain.exception.UserNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFound.class)
