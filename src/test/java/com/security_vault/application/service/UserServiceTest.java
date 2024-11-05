eadc08bef0e79185ac9176a1a3abc12022937413
Joao Gomes <jg0112360@gmail.com>
2024-11-05 17:14:50 -0300

    feat(test): new test configuration

---
+++ src/test/java/com/security_vault/application/service/UserServiceTest.java
@@ -0,0 +1,16 @@
+package com.security_vault.application.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void findOne() {
+    }
+
+    @Test
+    void createUser() {
+    }
+}
